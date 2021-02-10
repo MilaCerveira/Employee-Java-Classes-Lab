a2e7481be480d7286100ef506027efdb909147a8
fukiran <macdorphoto@yahoo.com>
2021-02-10 15:12:49 +0000

    manager tests passed

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,45 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import staff.management.Manager;
+
 public class ManagerTest {
+    Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("Sandy", 012345, 100000.00, "Senior Management");
+    }
+    @Test
+    public void hasName(){
+        assertEquals("Sandy", manager.getName());
+    }
+
+    @Test
+    public void hasNI() {
+        assertEquals(012345,manager.getNiNumber());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(100000.00, manager.getSalary(),0.01);
+    }
+
+    @Test
+    public void hasDeptName() {
+        assertEquals("Senior Management", manager.getDeptName());
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        manager.raiseSalary(500);
+        assertEquals(100500.00, manager.getSalary(),0.01);
+    }
+
+    @Test
+    public void canGetBonus() {
+        assertEquals(1000.00, manager.payBonus(),0.01);
+    }
 }
